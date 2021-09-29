423a0b6ac1ce24914a83d2cc9e5ab0fa8f32d101
tog22 <tomashoxford@gmail.com>
2021-09-29 13:38:17 -0700

    *** START VUE CODE IMPORT - State ***

--- GameWorld.js
+++ GameWorld.js
@@ -6,24 +6,15 @@ import './GameWorld.css';
 
 
 class GameWorld extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			of_the_world: [
-				[null,['angel','red',null],['angel','red','divinely inspired'],['angel','red',null],['angel','red',null],null],
-				[null,['mortal','red',null],['mortal','red',null],['mortal','red',null],['mortal','red',null],null],
-				[null,null,null,null,null,null],
-				[null,null,null,null,null,null],
-				[null,null,null,null,null,null],
-				[null,null,null,null,null,null],
-				[null,null,null,null,null,null],
-				[null,['mortal','black',null],['mortal','black',null],['mortal','black',null],['mortal','black',null],null],
-				[null,['angel','black',null],['angel','black',null],['angel','black','divinely inspired'],['angel','black',null],null]
-			]
-		};
-		this.render_row = this.render_row.bind(this);
+
+	render() {
+		const rows_array = this.state.otw.map(this.render_row);
 		
-		
+		return (
+			<table className="board">
+				{rows_array}
+			</table>
+		);
 	}
 	
 	render_row(row, row_index, array) {
@@ -38,11 +29,8 @@ class GameWorld extends React.Component {
 								key={square_key_name} 
 								row_index={row_index} 
 								column_index={column_index} 
-								contents={this.state.of_the_world[row_index][column_index]}
-								on_click={
-									this.square_click
-									// () => {this.square_click();}
-								}
+								contents={this.state.otw[row_index][column_index]}
+								on_click={this.square_click}
 							/>
 						)
 					}
@@ -51,33 +39,484 @@ class GameWorld extends React.Component {
 		);
 	}
 	
-	square_click (row_index, column_index) {
-		alert("Var passed to func in parent: "+row_index);
-	}
-	
-	render() {
-		const rows_array = this.state.of_the_world.map(this.render_row);
+	square_click (row, col) {
 		
-		return (
-			<table className="board">
-				{rows_array}
-			</table>
-		);
+		let clicked = this.state.otw[row][col];
+
 		
-		/*
-		let board_representation = [];
+		/********************
+		*********************
+		**				   **
+		**   VUE VERSION   **
+		**				   **
 		
-		for (var row = 0; row < this.state.of_the_world.length; row++) {
-			let row_to_add = [];
-			for (var column = 0; column < this.state.of_the_world[row].length; column++) {
-				let key_name = "square_"+row.toString()+"_"+column.toString();
-				row_to_add.push(<div key={key_name}>{row},{column}</div>);
-			}
-			board_representation.push(row_to_add);
+		let clicked = this.state.otw[row][col];
+		
+		let is_something_selected;
+		if (this.selected_row !== null && this.selected_col !== null) {
+			is_something_selected = true;
+		} else {
+			is_something_selected = false;
 		}
 		
-		return board_representation;
+		// 1) SELECTING PIECES
+		if (!is_something_selected) {
+			
+			// Check if the active side has a piece to select
+			
+			if (clicked.occupant === null) {
+				return;
+			}
+			
+			if (clicked.side !== this.current_player) {
+				alert ("It's not this player's turn");
+				// ↑ Ideally make the turn indicator flash red instead
+				return;
+			}
+			
+			// Check if the relevant thing (a mortal/angel, or divine inspiration) hasn't moved yet
+			
+			if (clicked.divinely_inspired && this.inspiration_has_moved) {
+				return;
+			}
+			
+			if (!clicked.divinely_inspired && this.piece_has_moved) {
+				return;
+			}
+			
+			// If conditions above are met, make the selection
+			
+			this.selected_row = row;
+			this.selected_col = col;
+			
+			this.state.otw[row][col].is_selected = 'selected ';
+			
+		// 2) MOVING SELECTED PIECES
+		} else if (is_something_selected) {
+			
+			let to_row = row;
+			let to_col = col;
+			let from_row = this.selected_row;
+			let from_col = this.selected_col;
+			
+			// Re-clicking the selected piece, to unselect it
+			
+			if (to_row === from_row && to_col === from_col) {
+				this.unselect_piece();
+			}
+			
+			
+			// Check if it's a valid move
+			// ...starting by calculating the deltas for later use
+			if (to_row > from_row) {
+				this.row_delta = to_row - from_row;
+			} else {
+				this.row_delta = from_row - to_row;
+			}
+			if (to_col > from_col) {
+				this.col_delta = to_col - from_col;
+			} else {
+				this.col_delta = from_col - to_col;
+			}
+			
+			let selected = this.state.otw[from_row][from_col];
+			
+			if (this.is_valid_move(this.selected_row, this.selected_col, to_row, to_col)) {
+				
+				// Make the move
+				
+				if (selected.divinely_inspired) {
+					this.inspiration_has_moved = true;
+					selected.divinely_inspired = false;
+					clicked.divinely_inspired = true;
+				} else if (selected.occupant === 'mortal') {
+					this.piece_has_moved = true;
+					selected.occupant = null;
+					selected.side = null;
+					clicked.occupant = 'mortal';
+					clicked.side = this.current_player;
+				} else if (selected.occupant === 'angel'){
+					this.piece_has_moved = true;
+					selected.occupant = null;
+					selected.side = null;
+					clicked.occupant = 'angel';
+					clicked.side = this.current_player;
+				}
+				
+				this.unselect_piece();
+				
+				// End turn/switch to the other player if appropriate
+				
+				if (this.piece_has_moved && this.inspiration_has_moved) {
+					this.end_turn();
+				}
+			
+			} else {
+				alert("Not a valid move");
+			}
+			
+		}
 		*/
+	}
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			current_player: 1,
+			piece_has_moved: false,
+			inspiration_has_moved: false,
+			selected_row: null,
+			selected_col: null,
+			row_delta: null,
+			col_delta: null,
+			otw: [
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 1,
+						divinely_inspired: true,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 1,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'mortal',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+				],
+				[
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 2,
+						divinely_inspired: true,
+						is_selected: ''
+					},
+					{
+						occupant: 'angel',
+						side: 2,
+						divinely_inspired: false,
+						is_selected: ''
+					},
+					{
+						occupant: null,
+						side: null,
+						divinely_inspired: false,
+						is_selected: ''
+					}
+				]
+			]
+		};
+		
+		this.render_row = this.render_row.bind(this);
+		
 	}
 }
 
